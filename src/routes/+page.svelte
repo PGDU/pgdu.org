b5c1a991439b9838ef185ac1d30c78020f1f4c76
David Micallef <david.micallef@coremarkets.co>
2025-02-10 21:37:57 +1100

    introducing baby elephant and introduce styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,36 @@
-<h1>Welcome to PgDU</h1>
-<p>
-	PostgreSQL Down Under Incorporated, known as PGDU, is a non for profit association. Our purpose is
-	to support the growth and education of PostgreSQL, The World's Most Advanced Open Source Database.
-</p>
-<p>
-	After successful conferences in Melbourne in 2018 and Sydney in 2019 we have now regrouped to hold
-	a conference on <b>Friday 17th of October 2025 in Sydney</b>. Stay tuned for venue and details to
-	follow.
-</p>
-<p>
-	Please send us an email at <a href="mailto:pgdu-committee@lists.pgdu.org"
-		>pgdu-committee@lists.pgdu.org</a
-	> if you have any questions.
-</p>
+<div class="container">
+	<img src="mstile-310x310.png" alt="baby elephent logo" style="width:5%;height:auto;" />
+
+	<h1>Welcome to PgDU</h1>
+	<p>
+		PostgreSQL Down Under Incorporated, known as PGDU, is a non for profit association. Our purpose
+		is to support the growth and education of PostgreSQL, The World's Most Advanced Open Source
+		Database.
+	</p>
+	<p>
+		After successful conferences in Melbourne in 2018 and Sydney in 2019 we have now regrouped to
+		hold a conference on <b>Friday 17th of October 2025 in Sydney</b>. Stay tuned for venue and
+		details to follow.
+	</p>
+	<p>
+		Please send us an email at <a href="mailto:pgdu-committee@lists.pgdu.org"
+			>pgdu-committee@lists.pgdu.org</a
+		> if you have any questions.
+	</p>
+</div>
 
 <style>
+	div.container {
+		margin: 5%;
+	}
+	h1 {
+		color: #1f9ac3;
+		font-family: 'Verdana';
+		font-size: 3em;
+	}
 	p {
 		color: black;
 		font-family: 'Ariel';
-		font-size: 2em;
+		font-size: 1em;
 	}
 </style>
